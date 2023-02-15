e900b0f3258706d249ca559fd7c39be8e534f3b0
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-15 11:52:54 +0100

    chore: set up tags files and page layout

---
+++ bms/tags/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$parent = 'tags/';
+$file = $parent;
+$page = 'Manage Tags';
+
+include __DIR__ . '/../header.php'; 
+?>
+
+    <!-- Content area -->
+    <div class="content pt-0">
+
+
+    </div>
+    <!-- /content area -->
+
+<?php include __DIR__ . '/../footer.php'; ?>
