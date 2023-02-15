efc17302df27a4784a9e171e670f1e9d63353f45
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-15 11:51:51 +0100

    chore: set up gallery files and page layout

---
+++ bms/gallery/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+$parent = 'gallery/';
+$file = $parent;
+$page = 'Media Manager';
+
+include __DIR__ . '/../header.php'; 
+?>
+
+    <!-- Content area -->
+    <div class="content pt-0">
+
+
+    </div>
+    <!-- /content area -->
+
+<?php include __DIR__ . '/../footer.php'; ?>
