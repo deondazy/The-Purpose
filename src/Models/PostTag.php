842783e5c37a3fd6cdbf9ad95b29de2828084a3f
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-23 19:11:25 +0100

    chore: update post tag model

--- src/Models/PostTag.php
+++ src/Models/PostTag.php
@@ -3,11 +3,39 @@
 namespace Core\Models;
 
 use Core\Models\Base;
+use Atlas\Query\Delete;
 
 class PostTag extends Base
 {
-    public function __construct()
+    protected $table = 'post_tags';
+
+    public function __construct($connection)
     {
-        parent::__construct('post_tags');
+        parent::__construct($connection, $this->table);
+    }
+
+    public function deleteRemovedTags($postId, $tags)
+    {
+        $delete = Delete::new($this->connection)
+            ->from($this->table)
+            ->whereEquals(['post_id' => $postId])
+            ->andWhereSprintf('tag_id NOT IN %s', $tags);
+        $delete->perform();
+    }
+
+    public function deleteAllTags($postId)
+    {
+        $delete = Delete::new($this->connection)
+            ->from($this->table)
+            ->whereEquals(['post_id' => $postId]);
+        $delete->perform();
+    }
+
+    public function saveTag($postId, $tagId)
+    {
+        return $this->create([
+            'post_id' => $postId,
+            'tag_id'  => $tagId
+        ]);
     }
 }
