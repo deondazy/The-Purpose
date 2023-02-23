4dcf9c47ba36256fe48a488e4e9cb8d15588a999
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-23 18:59:42 +0100

    bug: fix issue with bad file names making it not found

--- bms/http/gallery/upload.php
+++ bms/http/gallery/upload.php
@@ -3,44 +3,47 @@
 require_once __DIR__ . '/../../../bootstrap.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $target_dir = CORE_ROOT;
+    $targetDir = CORE_ROOT;
     $filePath = '/public/uploads/gallery/'.date('Y').'/'.date('m').'/'.date('d').'/';
 
-    $target_dir = $target_dir . $filePath;
+    $targetDir = $targetDir . $filePath;
 
-    if (!is_dir($target_dir)) {
-        mkdir($target_dir, 0755, true);
+    if (!is_dir($targetDir)) {
+        mkdir($targetDir, 0755, true);
     }
 
-    $uploaded_files = $_FILES['image-upload'];
+    $uploadedFiles = $_FILES['image-upload'];
 
-    $upload_success = true;
+    $uploadSuccess = true;
   
-    for ($i = 0; $i < count($uploaded_files['name']); $i++) {
-        $image = uniqid() . '_' . basename($uploaded_files['name'][$i]);
-        $target_file = $target_dir . $image;
-        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-        $check = getimagesize($uploaded_files['tmp_name'][$i]);
+    for ($i = 0; $i < count($uploadedFiles['name']); $i++) {
+        $image = basename($uploadedFiles['name'][$i]);
+        $imageFileType = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+        $newName = uniqid() . $imageFileType;
+        
+        $targetFile = $targetDir . $newName;
+
+        $check = getimagesize($uploadedFiles['tmp_name'][$i]);
   
         if ($check !== false) {
-            if (move_uploaded_file($uploaded_files['tmp_name'][$i], $target_file)) {
+            if (move_uploaded_file($uploadedFiles['tmp_name'][$i], $targetFile)) {
                 $gallery = new Core\Models\Gallery($connection);
                 $result = $gallery->create([
-                    'name' => $image,
-                    'image' => $filePath . $image,
+                    'name' => $newName,
+                    'image' => $filePath . $newName,
                     'user_id' => 1, // TODO: Use current user ID
                 ]);
             } else {
-                $error = 'Error uploading file ' . $target_file;
-                $upload_success = false;
+                $error = 'Error uploading file ' . $targetFile;
+                $uploadSuccess = false;
             }
         } else {
             $error = 'Invalid file format.';
-            $upload_success = false;
+            $uploadSuccess = false;
         }
     }
   
-    if ($upload_success) {
+    if ($uploadSuccess) {
         echo $result;
     } else {
         echo $error;
