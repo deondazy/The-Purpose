62f1d32d9d2ca6da79b400ff88bb19b440681789
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-23 19:10:05 +0100

    chore: update database schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,14 @@
+SET sql_mode = '';
+
 CREATE TABLE `categories` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `name` (`name`),
-  KEY `slug` (`slug`)
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `categories` (`id`, `name`, `slug`)
@@ -15,20 +18,25 @@ VALUES
 CREATE TABLE `comments` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `post_id` bigint(20) DEFAULT NULL,
-  `author_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Unspecified',
+  `author_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `author_email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `author_website` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `content` text COLLATE utf8mb4_unicode_ci,
-  `approved` tinyint(1) NOT NULL DEFAULT '0',
   `parent` bigint(20) NOT NULL DEFAULT '0',
+  `status` enum('PENDING','APPROVED','SPAM','TRASH') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'PENDING',
+  `status_history` enum('PENDING','APPROVED') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `user_id` bigint(20) NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `comment_post_id` (`post_id`),
-  KEY `comment_approved` (`approved`),
   KEY `comment_author_email` (`author_email`),
   KEY `comment_parent` (`parent`),
-  KEY `comment_date` (`created_at`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `comment_date` (`created_at`),
+  KEY `status` (`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `comments` (`id`, `post_id`, `author_name`, `author_email`, `author_website`, `content`, `status`) 
+VALUES (1, 1, 'Blog Commenter', 'commenter@email.example', 'https://example.example', 'Hi, this is a comment. To get started with moderating, editing, and deleting comments, please visit the Comments screen in the dashboard.', 'APPROVED')
 
 CREATE TABLE `gallery` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -37,7 +45,8 @@ CREATE TABLE `gallery` (
   `image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `post_categories` (
@@ -47,6 +56,9 @@ CREATE TABLE `post_categories` (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `post_categories` (`id`, `post_id`, `category_id`) 
+VALUES (1, 1, 1)
 
 CREATE TABLE `post_tags` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -64,14 +76,18 @@ CREATE TABLE `posts` (
   `featured_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `author` bigint(20) unsigned DEFAULT NULL,
   `status` enum('DRAFT','PUBLISH','TRASH') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'DRAFT',
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
-  KEY `post_title` (`title`),
+  UNIQUE KEY `slug` (`slug`),
+  UNIQUE KEY `title` (`title`),
   KEY `post_author` (`author`),
   KEY `post_status` (`status`),
   KEY `post_date` (`created_at`,`updated_at`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO `posts` (`id`, `title`, `slug`, `content`, `featured_image`, `author`, `status`, `created_at`, `updated_at`) 
+VALUES (1, 'Hello World', 'hello-world', '<p>Welcome to your Blog. This is your first post. Edit or delete it, then start writing!</p>', NULL, 1, 'PUBLISH', NOW(), NOW())
 
 CREATE TABLE `requests` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -88,15 +104,16 @@ CREATE TABLE `roles` (
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `permissions` longtext COLLATE utf8mb4_unicode_ci,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `roles` (`id`, `name`)
+INSERT INTO `roles` (`id`, `name`, `permissions`)
 VALUES
-	(1,'Administrator'),
-	(2,'Editor'),
-	(3,'Member'),
-	(4,'Guest');
+	(1, 'Administrator', {"post_create":"1","post_read":"1","post_edit":"1","post_delete":"1","post_publish":"1","category_create":"1","category_read":"1","category_edit":"1","category_delete":"1","tag_create":"1","tag_read":"1","tag_edit":"1","tag_delete":"1","comment_create":"1","comment_read":"1","comment_edit":"1","comment_delete":"1","gallery_create":"1","gallery_read":"1","gallery_edit":"1","gallery_delete":"1","user_create":"1","user_read":"1","user_edit":"1","user_delete":"1"}),
+	(2, 'Editor', {"post_create":"1","post_read":"1","post_edit":"1","post_delete":"1","post_publish":"1","tag_create":"1","tag_read":"1","tag_edit":"1","tag_delete":"1","comment_create":"1","comment_read":"1","comment_edit":"1","comment_delete":"1"}),
+	(3, 'Author', {"post_create":"1","post_read":"1","post_edit":"1","comment_create":"1","comment_read":"1"}),
+	(4, 'Reader', {"comment_create":"1"});
 
 CREATE TABLE `sessions` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -113,9 +130,11 @@ CREATE TABLE `tags` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `slug` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`)
+  UNIQUE KEY `name` (`name`),
+  UNIQUE KEY `slug` (`slug`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `user_roles` (
@@ -123,18 +142,24 @@ CREATE TABLE `user_roles` (
   `user_id` bigint(20) DEFAULT NULL,
   `role_id` bigint(20) DEFAULT 4,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `role_per_user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `first_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `last_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `display_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `bio` text COLLATE utf8mb4_unicode_ci,
   `avatar` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `user_email` (`email`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`),
   KEY `user_name` (`first_name`,`last_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
