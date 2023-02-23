2cc39ea549bca38962dd7c0437e9ad123bd3f3c8
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-23 18:18:16 +0100

    feat: add role model

---
+++ src/Models/Role.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace Core\Models;
+
+use Core\Models\Base;
+
+class Role extends Base
+{
+    protected $table = 'roles';
+
+    public function __construct($connection)
+    {
+        parent::__construct($connection, $this->table);
+    }
+}
