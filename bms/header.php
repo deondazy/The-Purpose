4fdfb837a8bdf548b62a1f0217b96abab9bfd406
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-27 18:19:23 +0100

    chore: update the backend header

--- bms/header.php
+++ bms/header.php
@@ -1,10 +1,11 @@
+<?php require_once __DIR__ . '/includes/bms-user.php'; ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr" class="layout-static">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title><?= $page ?> - Blog Management System</title>
+	<title><?= $page ?> ‹ The Purpose Center — BMS</title><!-- TODO: Change Hardcoded Site Name -->
 
 	<!-- Global stylesheets -->
 	<link href="<?= $config->site->url; ?>/bms/assets/fonts/inter/inter.css" rel="stylesheet" type="text/css">
