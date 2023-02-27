acaa327acf564d2752e6d6f45bdd397aae4d61d5
Deon Dazy <deondazy@Deons-MacBook-Pro.local>
2023-02-27 21:43:12 +0100

    chore: update header

--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,23 @@
 <?php 
-$session = new Core\Models\Session($connection);
 $user = new Core\Models\User($connection);
-$auth = new Core\Auth($user, $session);
 ?><!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>The Purpose Center</title>
+    <title>Purpose Center Chicago</title>
     <!-- favicons Icons -->
     <link rel="apple-touch-icon" sizes="180x180" href="<?= $config->site->url ?>/assets/images/Logo/logoblue.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="<?= $config->site->url ?>/assets/images/Logo/logoblue.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="<?= $config->site->url ?>/assets/images/logo/logoblue.png" />
     <link rel="manifest" href="<?= $config->site->url ?>/assets/images/Logo/logoblue.png" />
-    <meta name="description" content="Crsine HTML Template For Car Services" />
 
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link
         href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
-
 
     <link rel="stylesheet" href="<?= $config->site->url ?>/assets/vendors/bootstrap/css/bootstrap.min.css" />
     <link rel="stylesheet" href="<?= $config->site->url ?>/assets/vendors/animate/animate.min.css" />
@@ -51,7 +47,7 @@ $auth = new Core\Auth($user, $session);
     <div class="page-wrapper">
         <header class="main-header clearfix">
             <div class="main-header__logo">
-                <a class="" href="index.html">
+                <a class="" href="<?= $config->site->url ?>">
                     <img class="" width="100" src="<?= $config->site->url ?>/public/assets/images/icon.svg" alt="" />
                     <img class="" width="140" src="<?= $config->site->url ?>/public/assets/images/logo.svg" alt="" />
                 </a>
@@ -62,7 +58,7 @@ $auth = new Core\Auth($user, $session);
                         <div class="main-menu-wrapper__left">
                             <div class="main-menu-wrapper__left-content">
                                 <div class="main-menu-wrapper__left-text">
-                                    <p>Welcome to The Purpose Center</p>
+                                    <p>Welcome to Purpose Center</p>
                                 </div>
                                 <div class="main-menu-wrapper__left-email-box">
                                     <div class="icon">
